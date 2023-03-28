f4a382c613ead4ba3392ea42d64ec2e214bf5057
IceRayZer <icerayzer@gmail.com>
2023-03-28 10:45:43 +0200

    Organisation + Instanciate des routes (liste)

--- Assets/TeleportOnContact.cs
+++ Assets/TeleportOnContact.cs
@@ -4,6 +4,8 @@ public class TeleportOnContact : MonoBehaviour
 {
     public Transform teleportTarget; // The empty transform to teleport to
 
+    public GameObject[] routesListe; // Tableau de GameObjects
+
     private void OnTriggerEnter(Collider other)
     {
         // Check if the colliding object has the "Road" tag
@@ -13,6 +15,22 @@ public class TeleportOnContact : MonoBehaviour
             // Teleport the colliding object to the target position
             other.transform.position = teleportTarget.position;
             //Debug.Log("Teleported " + other.name);
+        }
+        // Vérifie si la liste n'est pas vide
+        if (routesListe.Length > 0 && teleportTarget != null)
+        {
+            // Génère un index aléatoire pour la liste
+            int randomIndex = Random.Range(0, routesListe.Length);
+
+            // Instancie le GameObject correspondant à l'index aléatoire
+            GameObject spawnedObject = Instantiate(routesListe[randomIndex], teleportTarget.position, teleportTarget.rotation);
+
+            // Optionnel : modifie le parent du GameObject instancié
+            spawnedObject.transform.parent = transform;
+        }
+        else
+        {
+            Debug.LogWarning("La liste de GameObjects ou le point de spawn est vide !");
         }
     }
 }
