6c86ed8036ca1287dd2d128a81eaca6d7b4f2051
Roneleven <erwan.levene@gmail.com>
2023-03-28 11:50:19 +0200

    pouet

--- Assets/RoadGenerator.cs
+++ Assets/RoadGenerator.cs
@@ -1,39 +1,76 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RoadGenerator : MonoBehaviour
 {
-    public GameObject[] roadPrefabs; // array of road prefabs to be used for generating the road
-    public int numRoadPieces; // number of road pieces to generate
-    public float roadLength; // length of each road piece
-    public Transform startMarker; // transform marking the start of the road
-    public Transform endMarker; // transform marking the end of the road
-    public float destroyDistance; // distance from the end marker at which a road piece is destroyed
+    public GameObject roadPrefab;
+    public GameObject player;
+    public int roadLength = 5;
+    public int segmentLength = 10;
+    public float distanceBeforeRegenerate = 50.0f;
 
-    private float nextSpawnDistance; // distance at which the next road piece will be spawned
-    private Transform player; // reference to the player transform
-    private GameObject lastRoadPiece; // reference to the last road piece spawned
+    private List<GameObject> segments;
+    private float distanceSinceLastSegment;
 
     private void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        nextSpawnDistance = roadLength;
-        SpawnRoadPiece(startMarker.position);
-    }
+        segments = new List<GameObject>();
 
-    private void Update()
-    {
-        if (player.position.z + destroyDistance > endMarker.position.z)
+        // Generate initial road segments
+        for (int i = 0; i < roadLength; i++)
         {
-            Destroy(lastRoadPiece);
-            SpawnRoadPiece(lastRoadPiece.transform.position + Vector3.forward * roadLength);
+            GenerateSegment();
         }
     }
+   void Update()
+{
+    // Get the player's current position
+    Vector3 playerPos = player.transform.position;
 
-    private void SpawnRoadPiece(Vector3 spawnPosition)
+    // Check if a new segment needs to be generated
+    if (playerPos.z > distanceSinceLastSegment - distanceBeforeRegenerate)
     {
-        GameObject roadPrefab = roadPrefabs[Random.Range(0, roadPrefabs.Length)];
-        GameObject roadPiece = Instantiate(roadPrefab, spawnPosition, Quaternion.identity);
-        lastRoadPiece = roadPiece;
-        nextSpawnDistance += roadLength;
+        // Generate a new segment
+        GenerateSegment();
+
+        // Check if the player has moved too far from the oldest segment
+        if (playerPos.z > distanceSinceLastSegment)
+        {
+            // If the player has moved too far, remove the oldest segment from the list and destroy it
+            Destroy(segments[0]);
+            segments.RemoveAt(0);
+            
+            // Move the new segment to be just ahead of the player
+            Vector3 offset = new Vector3(0, 0, segmentLength * (Mathf.FloorToInt(playerPos.z / segmentLength) + 1));
+            segments[segments.Count - 1].transform.position = offset;
+            
+            // Update the distanceSinceLastSegment variable
+            distanceSinceLastSegment = offset.z;
+        }
+    }
+}
+    private void GenerateSegment()
+    {
+        // Get the player's current position
+        Vector3 playerPos = player.transform.position;
+
+        // Calculate the position for the new segment
+        Vector3 segmentPos = new Vector3(0, 0, distanceSinceLastSegment);
+
+        // Add the new segment to the list
+        segments.Add(Instantiate(roadPrefab, segmentPos, Quaternion.identity));
+
+        // Update the distanceSinceLastSegment variable
+        distanceSinceLastSegment += segmentLength;
+
+        // Check if the player has moved too far from the new segment
+        if (playerPos.z > distanceSinceLastSegment)
+        {
+            // If the player has moved too far, move the segment to be just ahead of the player
+            Vector3 offset = new Vector3(0, 0, segmentLength * (Mathf.FloorToInt(playerPos.z / segmentLength) + 1));
+            segments[0].transform.position = offset;
+            segments.RemoveAt(0);
+            distanceSinceLastSegment = offset.z;
+        }
     }
 }
