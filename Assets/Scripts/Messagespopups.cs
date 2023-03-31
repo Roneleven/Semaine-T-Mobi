b291345d6a10e0fc10c7ad2ea6aa9b5675c8dded
Valdoris <tiganoeric@gmail.com>
2023-03-31 20:41:40 +0200

    Last FMOD Commit

--- Assets/Scripts/Messagespopups.cs
+++ Assets/Scripts/Messagespopups.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System.Runtime.CompilerServices;
+using DG.Tweening;
 
 public class Messagespopups : MonoBehaviour
 {
+    public Transform finalPos;
     public TruckMovement pommedeterre;
     private bool flag = true;
     public string[] listeMessages;
     TMP_Text textMessage;
     public float posYfinal = 860f;
-    private Vector3 posFinal;
     private float t;
     public float tempsDanim = 1f;
     private bool arreteAnim = false ;
     // Start is called before the first frame update
     void Start()
     {
-       textMessage = GetComponentInChildren<TMP_Text>();   
-       posFinal = new Vector3(transform.position.x, posYfinal, transform.position.z);   
+       textMessage = GetComponentInChildren<TMP_Text>();     
     }
 
     // Update is called once per frame
@@ -29,20 +29,7 @@ public class Messagespopups : MonoBehaviour
         if (pommedeterre.distanceTraveled >= 50 && flag)
         {
             flag = false;
-            //descendre l'affiche
-            /*if (arreteAnim)
-            {
-                t += Time.deltaTime;
-                float bruh = t / tempsDanim;
-                transform.position = Vector3.Lerp(transform.position, posFinal, tempsDanim);
-                if (bruh >= 1)
-                {
-                    transform.position = posFinal;
-                    t = 0;  
-                    arreteAnim = true;   
-                }
-            }*/
-            transform.position = posFinal;  
+            transform.DOMove(finalPos, 1f, false);
             //afficher le messages
             textMessage.text = listeMessages[Random.Range(0, listeMessages.Length)];
             //son de notif
