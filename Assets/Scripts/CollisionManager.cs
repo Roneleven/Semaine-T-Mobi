effd0b9675821369edd17d5d247df6a267e3911d
IceRayZer <icerayzer@gmail.com>
2023-04-01 05:10:57 +0200

    FMOD3

--- Assets/Scripts/CollisionManager.cs
+++ Assets/Scripts/CollisionManager.cs
@@ -2,25 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Lofelt.NiceVibrations;
+using FMODUnity;
 
 public class CollisionManager : MonoBehaviour
 {
 public new ParticleSystem particleSystem; // Assign the particle system component in the inspector
 public GameObject vehicle;
 
-void OnTriggerEnter(Collider other)
-{
-    if (other.CompareTag("Enemies"))
+    void OnTriggerEnter(Collider other)
     {
+    if (other.CompareTag("Enemies"))
+        {
         // Play the particle system
         if (particleSystem != null)
-        {
-            particleSystem.Play();
-            Destroy(other.gameObject);
-            Debug.Log("Vibration");
-            HapticPatterns.PlayPreset(HapticPatterns.PresetType.Failure);
-            Destroy(vehicle);
+            {
+                particleSystem.Play();
+                Destroy(other.gameObject);
+                Debug.Log("Vibration");
+                HapticPatterns.PlayPreset(HapticPatterns.PresetType.Failure);
+                FMODUnity.RuntimeManager.PlayOneShot("event:/Vehicule_Collision");
+                Destroy(vehicle);
+            }
         }
-    }    
-}
+    }
 }
